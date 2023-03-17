1211347799ee75bb09fd0963896c58cdf9240089
Samuel-Jonathan <jtn.samuel@gmail.com>
2023-03-17 01:49:18 +0100

    Affichage de la note totale

--- app/src/main/java/fr/samuel/qcm/RecapActivity.java
+++ app/src/main/java/fr/samuel/qcm/RecapActivity.java
@@ -1,10 +1,14 @@
 package fr.samuel.qcm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class RecapActivity extends AppCompatActivity {
 
@@ -13,15 +17,24 @@ public class RecapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recap);
 
+        double scoreTotal = 0;
+
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
 
         //Affichage du score
         for (int i = 0; i < Questionnaire.getInstance().getNbQuestions(); i++) {
-            itemsAdapter.add("Question n°"+i + ", score = " + Questionnaire.getInstance().getScore(i));
+            double score = Questionnaire.getInstance().getScore(i);
+            itemsAdapter.add("Question n°"+i + ", score = " + score);
+            scoreTotal += score;
         }
 
-        ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(itemsAdapter);
+
+        ListView scores = findViewById(R.id.scores);
+        scores.setAdapter(itemsAdapter);
+
+        //Affichage de la note totale
+        TextView noteTotale = findViewById(R.id.noteTotale);
+        noteTotale.setText("Note totale : " + scoreTotal);
 
     }
 }
